ba67f5d96fe70179e68e1fead5811759b6e9cecb
Theodore Itzaris <theodoreme@gmail.com>
2018-05-09 09:30:30 +0300

    add 6 blocks under the top image block

--- src/components/container.js
+++ src/components/container.js
@@ -6,26 +6,28 @@ import Message from './message'
 
 import './container.scss'
 
-const Container = (props) => {
+const Container = ({hasButton, hasMessage, source}) => {
   return (
     <div className="panel">
       <div className="panel--wrapper" >
-        <img src={props.source} />
-        <Button
+        <img src={source} />
+        {hasButton && <Button
           border='#fff'
           color='transparent'
           fontColor='#fff'
           text='Ask Demo'
-        />
-        <Message
+        />}
+        {hasMessage && <Message
           text='Add a message that you want to accompany the image'
-        />
+        />}
       </div>
     </div>
   )
 }
 
 Container.propTypes = {
+  hasButton: propTypes.bool,
+  hasMessage: propTypes.bool,
   link: propTypes.string,
   message: propTypes.string,
   source: propTypes.string,
--- src/components/main.js
+++ src/components/main.js
@@ -1,20 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Container from './container'
 
 import './main.scss'
 import img1 from '../assets/img/hd1080.png'
+import imgSmall from '../assets/img/300x300.png'
 
-const Main = () => {
-  return(
-    <main>
-      {/*main Image block*/}
-      <Container
+class Main extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      clients: [
+        {
+          id: 0,
+          name: 'client1',
+        },
+        {
+          id: 1,
+          name: 'client1',
+        },
+        {
+          id: 2,
+          name: 'client1',
+        },
+        {
+          id: 3,
+          name: 'client1',
+        },
+        {
+          id: 4,
+          name: 'client1',
+        },
+        {
+          id: 5,
+          name: 'client1',
+        },
+      ],
+    }
+  }
+
+  render(){
+    const clients = this.state.clients.map(client => {
+      return <Container
+        key={client.id}
         type="image"
-        source={img1}
-        message=""
-        link="" />
-    </main>
-  )
+        source={imgSmall}
+        message={client.name}
+        link=""
+      />
+    })
+    console.log(clients, this.state.clients) // eslint-disable-line
+    return(
+      <main>
+        {/*main Image block*/}
+        <Container
+          type="image"
+          source={img1}
+          message=""
+          link="" />
+        {/*clients blocks*/}
+        <div className="clientsBlock">
+          {clients}
+        </div>
+      </main>
+    )
+  }
 }
 
 export default Main
