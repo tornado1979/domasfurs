7d55aecc02380f5e31e8b0c6a3abedaec79e994d
Theodore Itzaris <theodoreme@gmail.com>
2018-05-19 11:42:13 +0300

    update galleryData & clientsData

--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,9 +1,9 @@
-import img1 from '../assets/img/Lorem-Ipsum2.jpg'
-import img2 from '../assets/img/Lorem-Ipsum2.jpg'
-import img3 from '../assets/img/Lorem-Ipsum2.jpg'
-import img4 from '../assets/img/Lorem-Ipsum2.jpg'
-import img5 from '../assets/img/Lorem-Ipsum2.jpg'
-import img6 from '../assets/img/Lorem-Ipsum2.jpg'
+import img1 from '../assets/img/pr1.jpg'
+import img2 from '../assets/img/pr2.jpg'
+import img3 from '../assets/img/pr3.jpg'
+import img4 from '../assets/img/pr4.jpg'
+import img5 from '../assets/img/pr5.jpg'
+import img6 from '../assets/img/pr6.jpg'
 
 export const clients = [
   {
--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -3,6 +3,17 @@ import img2 from '../assets/gallery/img2.jpg'
 import img3 from '../assets/gallery/img3.jpg'
 import img4 from '../assets/gallery/img4.jpg'
 import img5 from '../assets/gallery/img5.jpg'
+import img6 from '../assets/gallery/img6.jpg'
+import img7 from '../assets/gallery/img7.jpg'
+import img8 from '../assets/gallery/img8.jpg'
+import img9 from '../assets/gallery/img9.jpg'
+import img10 from '../assets/gallery/img10.jpg'
+import img11 from '../assets/gallery/img11.jpg'
+import img12 from '../assets/gallery/img12.jpg'
+import img13 from '../assets/gallery/img13.jpg'
+import img14 from '../assets/gallery/img14.jpg'
+import img15 from '../assets/gallery/img15.jpg'
+
 
 export const photos = [
   {
@@ -29,5 +40,55 @@ export const photos = [
     alt: 'Lorem Ipsum',
     id: 4,
     src: img5,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 5,
+    src: img6,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 6,
+    src: img7,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 7,
+    src: img8,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 8,
+    src: img9,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 9,
+    src: img10,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 10,
+    src: img11,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 11,
+    src: img12,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 12,
+    src: img13,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 13,
+    src: img14,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 14,
+    src: img15,
   },
 ]
