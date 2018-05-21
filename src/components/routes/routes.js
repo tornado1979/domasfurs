84e32623e2799fedee9153f6d51386e19b81fb27
Theodore Itzaris <theodoreme@gmail.com>
2018-05-21 09:47:49 +0300

    add routes

---
+++ src/components/routes/routes.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// import routing components
+import {
+  Route,
+  Switch,
+} from 'react-router-dom'
+
+import Main from '../../pages/main/main'
+import AboutUs from '../../pages/aboutus/aboutus'
+import Gallery from '../../pages/gallery/gallery'
+import ContactUs from '../../pages/contactus/contactus'
+
+export const Routes = () => {
+  return (
+    <Switch>
+      <Route
+        component={Main}
+        exact
+        path="/"
+      />
+      <Route
+        component={AboutUs}
+        path="/aboutus"
+      />
+      <Route
+        component={Gallery}
+        path="/gallery"
+      />
+      <Route
+        component={ContactUs}
+        path="/contact"
+      />
+    </Switch>
+  )
+}
