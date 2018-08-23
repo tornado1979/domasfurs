9e9d8a0e5978bcdaa1159c88d936a36e200d1eda
Theodoros Itzaris <theodoreme@gmail.com>
2018-08-23 10:00:06 +0300

    Add gallery images and update galleryData.js

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -3,7 +3,7 @@ import img2 from '../assets/gallery/img2.jpg'
 import img3 from '../assets/gallery/img3.jpg'
 import img4 from '../assets/gallery/img4.jpg'
 import img5 from '../assets/gallery/img5.jpg'
-/* import img6 from '../assets/gallery/img6.jpg'
+import img6 from '../assets/gallery/img6.jpg'
 import img7 from '../assets/gallery/img7.jpg'
 import img8 from '../assets/gallery/img8.jpg'
 import img9 from '../assets/gallery/img9.jpg'
@@ -12,7 +12,47 @@ import img11 from '../assets/gallery/img11.jpg'
 import img12 from '../assets/gallery/img12.jpg'
 import img13 from '../assets/gallery/img13.jpg'
 import img14 from '../assets/gallery/img14.jpg'
-import img15 from '../assets/gallery/img15.jpg' */
+import img15 from '../assets/gallery/img15.jpg'
+import img16 from '../assets/gallery/img16.jpg'
+import img17 from '../assets/gallery/img17.jpg'
+import img18 from '../assets/gallery/img18.jpg'
+import img19 from '../assets/gallery/img19.jpg'
+import img20 from '../assets/gallery/img20.jpg'
+import img21 from '../assets/gallery/img21.jpg'
+import img22 from '../assets/gallery/img22.jpg'
+import img23 from '../assets/gallery/img23.jpg'
+import img24 from '../assets/gallery/img24.jpg'
+import img25 from '../assets/gallery/img25.jpg'
+import img26 from '../assets/gallery/img26.jpg'
+import img27 from '../assets/gallery/img27.jpg'
+import img28 from '../assets/gallery/img28.jpg'
+import img29 from '../assets/gallery/img29.jpg'
+import img30 from '../assets/gallery/img30.jpg'
+import img31 from '../assets/gallery/img31.jpg'
+import img32 from '../assets/gallery/img32.jpg'
+import img33 from '../assets/gallery/img33.jpg'
+// no img34
+import img35 from '../assets/gallery/img35.jpg'
+import img36 from '../assets/gallery/img36.jpg'
+import img37 from '../assets/gallery/img37.jpg'
+import img38 from '../assets/gallery/img38.jpg'
+import img39 from '../assets/gallery/img39.jpg'
+import img40 from '../assets/gallery/img40.jpg'
+import img41 from '../assets/gallery/img41.jpg'
+import img42 from '../assets/gallery/img42.jpg'
+import img43 from '../assets/gallery/img43.jpg'
+import img44 from '../assets/gallery/img44.jpg'
+import img45 from '../assets/gallery/img45.jpg'
+import img46 from '../assets/gallery/img46.jpg'
+import img47 from '../assets/gallery/img47.jpg'
+import img48 from '../assets/gallery/img48.jpg'
+import img49 from '../assets/gallery/img49.jpg'
+import img50 from '../assets/gallery/img50.jpg'
+import img51 from '../assets/gallery/img51.jpg'
+import img52 from '../assets/gallery/img52.jpg'
+import img53 from '../assets/gallery/img53.jpg'
+import img54 from '../assets/gallery/img54.jpg'
+import img55 from '../assets/gallery/img55.jpg'
 
 
 export const photos = [
@@ -41,7 +81,7 @@ export const photos = [
     id: 4,
     src: img5,
   },
-/*  {
+  {
     alt: 'Lorem Ipsum',
     id: 5,
     src: img6,
@@ -90,5 +130,201 @@ export const photos = [
     alt: 'Lorem Ipsum',
     id: 14,
     src: img15,
-  }, */
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 15,
+    src: img16,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 16,
+    src: img17,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 17,
+    src: img18,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 18,
+    src: img19,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 19,
+    src: img20,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 20,
+    src: img21,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 21,
+    src: img22,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 22,
+    src: img23,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 23,
+    src: img24,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 24,
+    src: img25,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 25,
+    src: img26,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 26,
+    src: img27,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 27,
+    src: img28,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 28,
+    src: img29,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 29,
+    src: img30,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 30,
+    src: img31,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 31,
+    src: img32,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 32,
+    src: img33,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 34,
+    src: img35,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 35,
+    src: img36,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 36,
+    src: img37,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 37,
+    src: img38,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 38,
+    src: img39,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 39,
+    src: img40,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 40,
+    src: img41,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 41,
+    src: img42,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 42,
+    src: img43,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 43,
+    src: img44,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 44,
+    src: img45,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 45,
+    src: img46,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 46,
+    src: img47,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 47,
+    src: img48,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 48,
+    src: img49,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 49,
+    src: img50,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 50,
+    src: img51,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 51,
+    src: img52,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 52,
+    src: img53,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 53,
+    src: img54,
+  },
+  {
+    alt: 'Lorem Ipsum',
+    id: 54,
+    src: img55,
+  },
+
 ]
